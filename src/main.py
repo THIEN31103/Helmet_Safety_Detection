5c13877c37d6575cda6efc5cced63513d5a8f215
Duc Thien <thienngo086@gmail.com>
2025-07-30 15:31:09 +0700

    update

---
+++ src/main.py
@@ -0,0 +1,14 @@
+from ultralytics import YOLO
+
+MODEL_PATH = 'E:\AIO\Helmet_Safety_Detection\yolov10\yolov10n.pt'
+model = YOLO( MODEL_PATH )
+
+YAML_PATH ='E:\AIO\Helmet_Safety_Detection\Safety_Helmet_Dataset\data.yaml'
+EPOCHS = 1
+IMG_SIZE = 640
+BATCH_SIZE = 32
+
+model.train(data = YAML_PATH ,
+            epochs =EPOCHS ,
+            batch = BATCH_SIZE ,
+            imgsz = IMG_SIZE )
