61a760f3473eab5473f845db1013b0ccf4dc7691
Duc Thien <thienngo086@gmail.com>
2025-07-30 22:58:56 +0700

    update

--- src/main.py
+++ src/main.py
@@ -4,8 +4,8 @@ MODEL_PATH = 'E:\AIO\Helmet_Safety_Detection\yolov10\yolov10n.pt'
 model = YOLO( MODEL_PATH )
 
 YAML_PATH ='E:\AIO\Helmet_Safety_Detection\Safety_Helmet_Dataset\data.yaml'
-EPOCHS = 1
-IMG_SIZE = 640
+EPOCHS = 20
+IMG_SIZE = 64
 BATCH_SIZE = 32
 
 model.train(data = YAML_PATH ,
